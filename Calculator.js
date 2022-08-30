3c48d5a8d207d9c539fa5b6aa6c22f89b59e36b3
lno-Mier <108462758+lno-Mier@users.noreply.github.com>
2022-08-30 18:30:37 +0600

    Create Calculator.js

---
+++ Calculator.js
@@ -0,0 +1,44 @@
+let opp;
+let result;
+function Calculator() {
+	let num1 = Number(document.getElementById('num1').value);
+	let num2 = Number(document.getElementById('num2').value);
+	switch (opp) {
+		case '+':
+		result = num1 + num2
+		alert(result)
+		break;
+		case '-':
+		result = num1 - num2
+		alert(result)
+		break;
+		case '*':
+		result = num1 * num2
+		alert(result)
+		break;
+		case '/':
+		result = num1 / num2
+		alert(result)
+		break
+	}
+}
+let plus = document.getElementById('plus')
+plus.onclick = function() {
+	opp = '+'
+	alert(Calculator()[0])
+}
+let minus = document.getElementById('minus')
+minus.onclick = function() {
+	opp = '-'
+	alert(Calculator()[0])
+}
+let multiply = document.getElementById('multiply')
+multiply.onclick = function() {
+	opp = '*'
+	alert(Calculator()[0])
+}
+let divine = document.getElementById('divine')
+divine.onclick = function() {
+	opp = '/'
+	alert(Calculator()[0])
+}
